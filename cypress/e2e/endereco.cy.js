b5eb042eeb139150fd13b4f33289bc5c436c45e3
Karina <karimacedog@gmail.com>
2023-11-10 11:06:27 -0300

    Usando Page Objects

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,15 +1,17 @@
 /// <reference types = "cypress" />
+import enderecoPage from '../support/page-objects/endereco.page'
 
 describe('Funcionalidade Endereços de Faturamento e Entrega', () => {
 
     beforeEach(() => {
         cy.visit('minha-conta/')
-        cy.fixture('perfil').then(dados =>{
+        cy.fixture('perfil').then(dados => {
             cy.login(dados.usuario, dados.senha)
         })
     });
 
-    it('Fazer cadastro de faturamento com sucesso', () => {
-
+    it.only('Fazer cadastro de faturamento com sucesso', () => {
+        enderecoPage.editarEnderecoFaturamento('Karina', 'Macedo', 'Folia', 'Brasil', 'Av Senador', '1004', 'São Paulo', 'São Paulo', '02248790', '946325879', 'kari@folia.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
     });
 });
